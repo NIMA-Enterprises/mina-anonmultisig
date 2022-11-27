8eda254c13314be77e072e21347b64511d9e275f
markolazic01 <marko@2key.co>
2022-11-27 11:55:26 +0100

    Implement basic signature mechanism and admin authority

--- src/AnonMultiSig.ts
+++ src/AnonMultiSig.ts
@@ -1,26 +1,25 @@
 import {
   Field,
   SmartContract,
+  Bool,
   state,
   State,
   method,
   DeployArgs,
   Permissions,
   PrivateKey,
+  Signature,
+  PublicKey,
+  Circuit,
+  CircuitString
 } from 'snarkyjs';
 
-/**
- * Basic Example
- * See https://docs.minaprotocol.com/zkapps for more info.
- *
- * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
- * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
- *
- * This file is safe to delete and replace with your own contract.
- */
 export class AnonMultiSig extends SmartContract {
   @state(Field) num = State<Field>();
-
+  @state(Field) nonce = State<Field>();
+  @state(Field) root = State<Field>();
+  @state(PublicKey) admin = State<PublicKey>();
+  
   deploy(args: DeployArgs) {
     super.deploy(args);
     this.setPermissions({
@@ -35,12 +34,33 @@ export class AnonMultiSig extends SmartContract {
     this.requireSignature();
   }
 
-  @method update() {
-    const currentState = this.num.get();
-    this.num.assertEquals(currentState); // precondition that links this.num.get() to the actual on-chain state
-    const newState = currentState.add(2);
-    newState.assertEquals(currentState.add(2));
-    this.num.set(newState);
+  @method setAdmin(admin: PublicKey) {
+    const currentAdmin: PublicKey = this.admin.get();
+    this.admin.assertEquals(currentAdmin);
+    this.admin.set(Circuit.if(currentAdmin.isEmpty(), admin, currentAdmin));
     this.requireSignature();
+  }
+
+  // Make sure admin signed the update value
+  @method update(signature: Signature, num: Field) {
+    // Make sure admin is set
+    const admin = this.admin.get();
+    this.admin.assertEquals(admin);
+    const isAdminSet: Bool = admin.isEmpty();
+    isAdminSet.assertTrue;
+    // Secure states
+    const currentStateNum: Field = this.num.get();
+    this.num.assertEquals(currentStateNum);
+    const currentStateNonce: Field = this.nonce.get();
+    this.nonce.assertEquals(currentStateNonce);
+    // Compute signed message
+    const msg: CircuitString = CircuitString.fromString(num.toString().concat(currentStateNonce.toString()));
+    const msgHash: Field = msg.hash();
+    // Verify signature
+    const success = signature.verify(admin, msgHash.toFields());
+    success.assertTrue;
+    // Set new states
+    this.nonce.set(currentStateNonce.add(1));
+    this.num.set(num);
   }
 }
