2a1b4e39263708b5d48d4e68cd703a2e683880ce
markolazic01 <marko@2key.co>
2022-11-28 11:46:10 +0100

    Introduce complete initialization and permission transfering flow

--- src/AnonMultiSig.ts
+++ src/AnonMultiSig.ts
@@ -33,39 +33,79 @@ export class AnonMultiSig extends SmartContract {
     });
   }
 
-  @method init(zkappKey: PrivateKey) {
-    super.init(zkappKey);
-    this.num.set(Field(1));
-    this.requireSignature();
-  }
+  /**
+   * @notice Function to initialize 'AnonMultiSig' smart contract
+   * @param admin is public key of initial administrator
+   * @param membersTreeRoot is root of a merkle tree containing all members
+   * @param numberOfMembers is number of members contained in the tree
+   * @param minimalQuorum is minimal amount of votes needed to execute/cancel proposal
+   * @dev 182 is maximum number of votes that can be fit into single word of memory
+   */
+  @method initialize(admin: PublicKey, membersTreeRoot: Field, numberOfMembers: Field, minimalQuorum: Field) {
+    // Set root
+    const currentMembersTreeRoot: Field = this.membersTreeRoot.get();
+    this.membersTreeRoot.assertEquals(currentMembersTreeRoot);
+    currentMembersTreeRoot.assertEquals(Field(0));
+    membersTreeRoot.assertGt(Field(0));
+    this.membersTreeRoot.set(membersTreeRoot);
 
-  @method setAdmin(admin: PublicKey) {
+    // Set admin
     const currentAdmin: PublicKey = this.admin.get();
     this.admin.assertEquals(currentAdmin);
-    this.admin.set(Circuit.if(currentAdmin.isEmpty(), admin, currentAdmin));
+    currentAdmin.isEmpty().assertTrue();
+    admin.isEmpty().assertFalse();
+    this.admin.set(admin);
+
+    // Set initial number of members
+    const currentNumberOfMembers: Field = this.numberOfMembers.get();
+    this.numberOfMembers.assertEquals(currentNumberOfMembers);
+    currentNumberOfMembers.assertEquals(Field(0));
+    numberOfMembers.assertGt(Field(0));
+    numberOfMembers.assertLte(Field(182));
+    this.numberOfMembers.set(Field(numberOfMembers));
+
+    // Set minimal quorum
+    const currentMinimalQuorum: Field = this.minimalQuorum.get();
+    this.minimalQuorum.assertEquals(currentMinimalQuorum);
+    currentMinimalQuorum.assertEquals(Field(0));
+    minimalQuorum.assertGt(Field(0));
+    minimalQuorum.assertLt(numberOfMembers);
+    this.minimalQuorum.set(minimalQuorum);
+
+    // Require zkApp signature
     this.requireSignature();
   }
 
-  // Make sure admin signed the update value
-  @method update(signature: Signature, num: Field) {
-    // Make sure admin is set
-    const admin = this.admin.get();
-    this.admin.assertEquals(admin);
-    const isAdminSet: Bool = admin.isEmpty();
-    isAdminSet.assertTrue;
-    // Secure states
-    const currentStateNum: Field = this.num.get();
-    this.num.assertEquals(currentStateNum);
-    const currentStateNonce: Field = this.nonce.get();
-    this.nonce.assertEquals(currentStateNonce);
-    // Compute signed message
-    const msg: CircuitString = CircuitString.fromString(num.toString().concat(currentStateNonce.toString()));
-    const msgHash: Field = msg.hash();
-    // Verify signature
-    const success = signature.verify(admin, msgHash.toFields());
-    success.assertTrue;
-    // Set new states
-    this.nonce.set(currentStateNonce.add(1));
-    this.num.set(num);
+  /**
+   * @notice Function to set new 'AnonMultiSig' administrator
+   * @param admin is public key of new administrator
+   * @param signature is expireable signature provided by the current admin
+   * @param expirationTimestamp is timestamp until which signature is valid
+   */
+  @method setAdmin(admin: PublicKey, signature: Signature, expirationTimestamp: UInt64) {
+    // Get and assert current admin
+    const currentAdmin: PublicKey = this.admin.get();
+    this.admin.assertEquals(currentAdmin);
+
+    // Require that new admin is not empty
+    admin.isEmpty().assertFalse();
+    // Require new admin is different than the current one
+    currentAdmin.equals(admin).assertFalse();
+
+    // Reconstruct signed message
+    const msg: Field = CircuitString.fromString(
+      admin.toString().concat(expirationTimestamp.toString())
+    ).hash();
+
+    // Make sure signature is valid
+    const isSignatureValid: Bool = signature.verify(currentAdmin, msg.toFields());
+    isSignatureValid.assertTrue;
+
+    // Require signature has not expired
+    this.network.timestamp.assertBetween(UInt64.from(0) , expirationTimestamp);
+
+    // Set new admin
+    this.admin.set(admin);
   }
+
 }
