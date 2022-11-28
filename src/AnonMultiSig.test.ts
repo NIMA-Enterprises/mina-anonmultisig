9c859caa229afebca6411c45125ba55004b36244
markolazic01 <marko@2key.co>
2022-11-28 11:50:25 +0100

    Adapt tests to new structures

--- src/AnonMultiSig.test.ts
+++ src/AnonMultiSig.test.ts
@@ -10,27 +10,31 @@ import {
   Signature,
   CircuitString,
   Bool,
+  UInt64,
 } from 'snarkyjs';
 
 let proofsEnabled = false;
-function createLocalBlockchain() {
+function createLocalBlockchain(): PrivateKey[] {
   const Local = Mina.LocalBlockchain({ proofsEnabled });
   Mina.setActiveInstance(Local);
-  return Local.testAccounts[0].privateKey;
+  return [
+    Local.testAccounts[0].privateKey, 
+    Local.testAccounts[1].privateKey, 
+    Local.testAccounts[2].privateKey
+  ];
 }
 
 async function localDeploy(
   zkAppInstance: AnonMultiSig,
-  zkAppPrivatekey: PrivateKey,
+  zkAppPrivateKey: PrivateKey,
   deployerAccount: PrivateKey
 ) {
   const txn = await Mina.transaction(deployerAccount, () => {
     AccountUpdate.fundNewAccount(deployerAccount);
-    zkAppInstance.deploy({ zkappKey: zkAppPrivatekey });
-    zkAppInstance.init(zkAppPrivatekey);
+    zkAppInstance.deploy({ zkappKey: zkAppPrivateKey });
   });
   await txn.prove();
-  txn.sign([zkAppPrivatekey]);
+  txn.sign([zkAppPrivateKey]);
   await txn.send();
 }
 
@@ -39,12 +43,14 @@ describe('AnonMultiSig', () => {
     zkAppAddress: PublicKey,
     zkAppPrivateKey: PrivateKey,
     zkAppInstance: AnonMultiSig;
+  
+  let account1: PrivateKey, account2: PrivateKey;
 
   beforeAll(async () => {
     await isReady;
     if (proofsEnabled) AnonMultiSig.compile();
 
-    deployerAccount = createLocalBlockchain();
+    [deployerAccount, account1, account2] = createLocalBlockchain();
     zkAppPrivateKey = PrivateKey.random();
     zkAppAddress = zkAppPrivateKey.toPublicKey();
     zkAppInstance = new AnonMultiSig(zkAppAddress);
@@ -57,64 +63,72 @@ describe('AnonMultiSig', () => {
     setTimeout(shutdown, 0);
   });
 
-  it('generates and deploys the `AnonMultiSig` smart contract', async () => {
-    // Deploy zkApp
-    await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
-    // Make sure zkApp is initialized
-    const num = zkAppInstance.num.get();
-    expect(num).toEqual(Field(1));
-  });
-
-  it('correctly sets the admin state', async () => {
-    // Create and send transaction
-    const txn = await Mina.transaction(deployerAccount, () => {
-      zkAppInstance.setAdmin(deployerAccount.toPublicKey());
+  describe("General flow tests", () => {
+    it('generates and deploys the `AnonMultiSig` smart contract', async () => {
+      // Deploy zkApp
+      await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
     });
-    await txn.prove();
-    txn.sign([zkAppPrivateKey]);
-    await txn.send();
-    // console.log(zkAppInstance.admin.get().x.toString()); // Log the admin address
-    // Make sure admin is set
-    const admin = zkAppInstance.admin.get();
-    expect(admin).toEqual(deployerAccount.toPublicKey());
-  });
-
-  it('correctly updates the num state', async () => {
-    const num: Field = Field(7);
-    const nonce: Field = zkAppInstance.nonce.get();
-
-    // Generate signature
-    const msg: CircuitString = CircuitString.fromString(num.toString().concat(nonce.toString()));
-    const sig: Signature = Signature.create(deployerAccount, msg.hash().toFields());
-
-    // Create and send transaction
-    const txn = await Mina.transaction(deployerAccount, () => {
-      zkAppInstance.update(sig, num);
+  
+    it('correctly initializes `AnonMultiSig` smart contract', async () => {
+      // Given
+      const admin: PublicKey = deployerAccount.toPublicKey();
+      const demoRoot: Field = Field(1);
+      const numberOfMembers: Field = Field(4);
+      const minimalQuorum: Field = Field(3);
+  
+      // When
+      const txn = await Mina.transaction(deployerAccount, () => {
+        zkAppInstance.initialize(admin, demoRoot, numberOfMembers, minimalQuorum);
+      });
+      await txn.prove();
+      txn.sign([zkAppPrivateKey]);
+      await txn.send();
+  
+      // Then
+      expect(zkAppInstance.admin.get()).toEqual(admin);
+      expect(zkAppInstance.membersTreeRoot.get()).toEqual(demoRoot);
+      expect(zkAppInstance.numberOfMembers.get()).toEqual(numberOfMembers);
+      expect(zkAppInstance.minimalQuorum.get()).toEqual(minimalQuorum);
     });
-    await txn.prove();
-    txn.sign([zkAppPrivateKey]);
-    await txn.send();
-
-    // Check if state is updated properly
-    const updatedNum = zkAppInstance.num.get();
-    expect(updatedNum).toEqual(Field(7));
+  
+    it('correctly sets the new admin', async () => {
+      // Given
+      const newadmin: PublicKey = account1.toPublicKey();
+      // Expiration timestamp which is valid for 2 minutes after created
+      const expirationTimestamp: UInt64 = Mina.getNetworkState().timestamp.add(UInt64.from(120));
+      // Compute message hash
+      const msgHash: Field = CircuitString.fromString(newadmin.toString().concat(expirationTimestamp.toString())).hash();
+      // Deployer is current admin
+      const signature: Signature = Signature.create(deployerAccount, msgHash.toFields());
+  
+      // Create and send transaction
+      const txn = await Mina.transaction(deployerAccount, () => {
+        zkAppInstance.setAdmin(newadmin, signature, expirationTimestamp);
+      });
+      await txn.prove();
+      txn.sign([zkAppPrivateKey]);
+      await txn.send();
+  
+      // Then
+      expect(zkAppInstance.admin.get()).toEqual(newadmin);
+    });
   });
 
   describe('General POC tests', () => {
     it('Create & Recover signature with snarkyjs', async () => {
       const privateKey: PrivateKey = PrivateKey.random();
       const publicKey: PublicKey = privateKey.toPublicKey();
-  
+
       const msg: CircuitString = CircuitString.fromString('hello');
       const msgHash: Field = msg.hash();
       // console.log(msg, msg.toFields(), msgHash, msgHash.toString());
-  
+
       const sig: Signature = Signature.create(privateKey, msgHash.toFields());
       // console.log(sig, sig.r.toString(), sig.s.toString());
-  
+
       const success: Bool = sig.verify(publicKey, msgHash.toFields());
       // console.log(success);
-  
+
       expect(success).toBeTruthy;
     });
   });
