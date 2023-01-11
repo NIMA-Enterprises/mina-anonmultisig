9fcd72f3c4e71ba122c9f87763829a34ab7c4a2f
markolazic01 <marko@2key.co>
2023-01-11 11:16:01 +0100

    Add reset mock script

---
+++ src/resetMock.ts
@@ -0,0 +1,39 @@
+import { AnonMultiSig } from './AnonMultiSigMock.js';
+import * as AnonMultiSigLib from './AnonMultiSigLib.js';
+import * as dotenv from 'dotenv';
+import { Mina, isReady, PrivateKey, shutdown } from 'snarkyjs';
+
+dotenv.config({ path: './.env' });
+
+await isReady;
+
+const dpk: string = process.env.DPK || '';
+const deployerPrivateKey: PrivateKey = PrivateKey.fromBase58(dpk);
+
+let Berkeley = Mina.Network('https://proxy.berkeley.minaexplorer.com/graphql');
+
+Mina.setActiveInstance(Berkeley);
+
+const zpk: string = process.env.MZPK || '';
+const zkAppPrivateKey: PrivateKey = PrivateKey.fromBase58(zpk);
+
+const zkAppInstance: AnonMultiSig = new AnonMultiSig(
+  zkAppPrivateKey.toPublicKey()
+);
+
+// When
+const txn = await Mina.transaction(
+  {
+    feePayerKey: deployerPrivateKey,
+    fee: AnonMultiSigLib.TX_FEE,
+    memo: 'Reset',
+  },
+  () => {
+    zkAppInstance.reset();
+  }
+);
+await txn.prove();
+txn.sign([zkAppPrivateKey]);
+await txn.send();
+
+shutdown();
