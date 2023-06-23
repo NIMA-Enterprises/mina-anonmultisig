d44e5f1f5fa1470ff08e2b435e0db4bcc110a89c
AstralBlip <astralblip@gmail.com>
2023-06-23 14:45:46 +0200

    Update manual deployment script

--- packages/contracts/src/manualDeploy.ts
+++ packages/contracts/src/manualDeploy.ts
@@ -1,14 +1,12 @@
 import { AnonMultiSigMock } from './AnonMultiSigMock.js';
 import * as AnonMultiSigLib from './AnonMultiSigLib.js';
 import * as dotenv from 'dotenv';
-import { Mina, isReady, PrivateKey, shutdown } from 'snarkyjs';
+import { Mina, PrivateKey, PublicKey } from 'snarkyjs';
 
 dotenv.config({ path: './.env' });
 
-await isReady;
-
-const dpk: string = process.env.DPK || '';
-const deployerPrivateKey: PrivateKey = PrivateKey.fromBase58(dpk);
+//const dpk: string = process.env.DPK || '';
+//const deployerPrivateKey: PrivateKey = PrivateKey.fromBase58(dpk);
 
 let Berkeley = Mina.Network('https://proxy.berkeley.minaexplorer.com/graphql');
 
@@ -16,24 +14,31 @@ Mina.setActiveInstance(Berkeley);
 
 const zpk: string = process.env.MZPK || '';
 const zkAppPrivateKey: PrivateKey = PrivateKey.fromBase58(zpk);
+const zkAppPublicKey: PublicKey = zkAppPrivateKey.toPublicKey();
 
-const zkAppInstance: AnonMultiSigMock = new AnonMultiSigMock(
-  zkAppPrivateKey.toPublicKey()
-);
+console.log('Compiling...');
+console.time('compile');
+let { verificationKey } = await AnonMultiSigMock.compile();
+console.timeEnd('compile');
 
-// When
-const txn = await Mina.transaction(
+// produce and log the transaction json; the fee payer is a dummy which has to be added later, by the signing logic
+let tx = await Mina.transaction(
   {
-    sender: deployerPrivateKey.toPublicKey(),
+    sender: zkAppPublicKey,
     fee: AnonMultiSigLib.TX_FEE,
     memo: 'Deploy',
   },
   () => {
-    zkAppInstance.deploy({ zkappKey: zkAppPrivateKey });
+    new AnonMultiSigMock(zkAppPublicKey).deploy({
+      verificationKey,
+    });
   }
 );
-await txn.prove();
-txn.sign([deployerPrivateKey]);
-await txn.send();
-
-shutdown();
+console.log('Proving...');
+console.time('prove');
+await tx.prove();
+console.timeEnd('prove');
+let transactionJson = tx.sign([zkAppPrivateKey]).toJSON();
+console.log(transactionJson);
+await tx.send();
+console.log('Tx sent!');
